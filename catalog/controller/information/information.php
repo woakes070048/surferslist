8b69600ebe6c1e9c09e57ee80ef6b500a1258015
garudacrafts <derek@garudacrafts.com>
2019-03-15 11:47:34 -0400

    hotfix syntax error info pages

--- catalog/controller/information/information.php
+++ catalog/controller/information/information.php
@@ -29,7 +29,7 @@ class ControllerInformationInformation extends Controller {
 
 			$this->data['description'] = html_entity_decode($information_info['description'], ENT_QUOTES, 'UTF-8');
 
-			$this->data['continue'] = $this->url->link('common/home'$this->cache->delete('manufacturer'););
+			$this->data['continue'] = $this->url->link('common/home');
 			$this->data['page'] = $this->url->link('information/information', 'information_id=' .  $information_id);
 
 			$this->model_catalog_information->updateViewed($this->request->get['information_id']);
